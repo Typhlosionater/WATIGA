bf0b55c16e848d44500ff64c1d292c680ef4d261
stormytuna <stormytuna@outlook.com>
2025-06-17 01:56:42 +0100

    Add soul storage device

---
+++ Content/ExpertAccessories/SoulStorageDevice.cs
@@ -0,0 +1,66 @@
+using Terraria.DataStructures;
+using Terraria.GameContent.ItemDropRules;
+using WATIGA.Common;
+using Terraria.ID;
+
+namespace WATIGA.Content.ExpertAccessories;
+
+public class SoulStorageDevice : ModItem
+{
+	public override void SetDefaults() {
+		Item.width = 34;
+		Item.height = 30;
+		Item.value = Item.sellPrice(0, 10, 0, 0);
+		Item.rare = ItemRarityID.Expert;
+		Item.accessory = true;
+
+		Item.expert = true;
+		Item.expertOnly = true;
+	}
+
+	public override void UpdateAccessory(Player player, bool hideVisual) {
+		player.GetModPlayer<SoulStorageDevicePlayer>().Active = true;
+	}
+}
+
+public class SoulStorageDevicePlayer : ModPlayer
+{
+	public bool Active;
+
+	public override void ResetEffects() {
+		Active = false;
+	}
+
+	public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genDust, ref PlayerDeathReason damageSource) {
+		if (!Active || Player.HasBuff<SoulStorageDeviceCooldown>()) {
+			return base.PreKill(damage, hitDirection, pvp, ref playSound, ref genDust, ref damageSource);
+		}
+
+		Player.Heal(Player.statLifeMax2 / 2);
+		Player.AddBuff(ModContent.BuffType<SoulStorageDeviceCooldown>(), 3 * 60 * 60);
+		
+		return false;
+	}
+}
+
+public class SoulStorageDeviceCooldown : ModBuff
+{
+	public override void SetStaticDefaults() {
+		Main.debuff[Type] = true;
+	}
+}
+
+public class SoulStorageDeviceDrop : GlobalItem
+{
+	public override bool IsLoadingEnabled(Mod mod) {
+		return ServerConfig.Instance.NewContent.ExpertAccessories;
+	}
+
+	public override bool AppliesToEntity(Item entity, bool lateInstantiation) {
+		return entity.type == ItemID.TwinsBossBag;
+	}
+
+	public override void ModifyItemLoot(Item item, ItemLoot itemLoot) {
+		itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<SoulStorageDevice>()));
+	}
+}
